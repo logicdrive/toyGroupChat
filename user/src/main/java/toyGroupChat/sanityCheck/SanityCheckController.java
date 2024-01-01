774e8babcddd7082ba61454dcf75f43c71e5e689
logicdrive <logicdrive7@gmail.com>
2024-01-01 10:31:33 +0900

    Feat: SanityCheck 응답 상태를 변경할 수 있게됨

--- user/src/main/java/toyGroupChat/sanityCheck/SanityCheckController.java
+++ user/src/main/java/toyGroupChat/sanityCheck/SanityCheckController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import lombok.RequiredArgsConstructor;
 import toyGroupChat._global.logger.CustomLogger;
@@ -18,13 +19,34 @@ import toyGroupChat.sanityCheck.resDtos.LogsResDto;
 @RequestMapping("/sanityCheck")
 public class SanityCheckController {
     private final SanityCheckService sanityCheckService;
+    private boolean isNormalSanityCheck = true;
 
     // 정상적인 통신 여부를 단순하게 확인해보기 위해서
     @GetMapping
-    public void sanityCheck() {
+    public ResponseEntity<Void> sanityCheck() {
+        CustomLogger.debug(CustomLoggerType.ENTER_EXIT, "",
+            String.format("{isNormalSanityCheck: %b}", this.isNormalSanityCheck));
+        
+        return ResponseEntity.status((this.isNormalSanityCheck ? HttpStatus.OK : HttpStatus.INTERNAL_SERVER_ERROR))
+            .build();
+    }
+
+    // 에러 복구 여부 테스트를 위해서 SanityCheck시에 무조건 정상 처리가 반환되도록 만듬
+    @PutMapping("/setNormal")
+    public void sanityCheckSetNormal() {
+        this.isNormalSanityCheck = true;
         CustomLogger.debug(CustomLoggerType.ENTER_EXIT);
         return;
     }
+
+    // 에러 복구 여부 테스트를 위해서 SanityCheck시에 무조건 내부 서버 에러가 반환되도록 만듬
+    @PutMapping("/setError")
+    public void sanityCheckSetError() {
+        this.isNormalSanityCheck = false;
+        CustomLogger.debug(CustomLoggerType.ENTER_EXIT);
+        return;
+    }
+
 
     // 현재 저장된 로그들 중에서 일부분을 간편하게 가져오기 위해서
     @GetMapping("/logs")
