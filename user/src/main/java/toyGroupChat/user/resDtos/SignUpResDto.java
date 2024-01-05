ee68d5a35bce8cc23095d625bd777491139f7e1c
logicdrive <logicdrive7@gmail.com>
2024-01-05 21:35:10 +0900

    Fix: SignUp 반한시에 id가 함께 반환되도록 수정

--- user/src/main/java/toyGroupChat/user/resDtos/SignUpResDto.java
+++ user/src/main/java/toyGroupChat/user/resDtos/SignUpResDto.java
@@ -7,10 +7,12 @@ import toyGroupChat.domain.User;
 @Getter
 @ToString
 public class SignUpResDto {
+    private final Long id;
     private final String email;
     private final String name;
 
     public SignUpResDto(User user) {
+        this.id = user.getId();
         this.email = user.getEmail();
         this.name = user.getName();
     }
